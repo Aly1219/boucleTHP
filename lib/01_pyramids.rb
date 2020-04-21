fe47e1c58363c306d9bbcd0a17c9581012740901
Aly1219 <alissunline@gmail.com>
2020-04-21 15:23:13 +0200

    fucking pyramids!

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -36,8 +36,39 @@ def full_pyramid
 
 end 
 
+def wtf_pyramid
+    puts "Salut, bienvenue dans ma super pyramide!"
+    puts "Combien d'étages veux-tu?"
+    print">"
+    stares=gets.chomp.to_i
+
+    aste="#"
+    sky=stares-1
+    length=1
+
+    while(sky>=0)
+        print " "*sky
+        print aste*length
+        puts " "*sky
+        length+=2
+        sky-=1
+    end
+
+    doing=stares-1
+
+    doing.times do
+        print " " * sky
+        print aste*length
+        puts " "*sky
+        
+        sky+=1
+        legth-=2
+    end
+end
+
+
 def perform
-    full_pyramid
+    wtf_pyramid
 end
 
 perform

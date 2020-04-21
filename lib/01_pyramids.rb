606f9320cfdf48e67b86a8ee3629ea7d62f5b2c9
Aly1219 <alissunline@gmail.com>
2020-04-21 14:37:33 +0200

    add exo 02

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,43 @@
+def half_pyramid
+
+    puts "Salut, bienvenue dans ma super pyramide!"
+    puts "Combien d'étages veux-tu?"
+    print">"
+    stares=gets.chomp.to_i
+
+    aste="#"
+    sky=stares-1
+
+    while(sky>=0)
+        print " "*sky
+        puts aste
+        aste+="#"
+        sky-=1
+    end
+
+end
+
+def full_pyramid
+    puts "Salut, bienvenue dans ma super pyramide!"
+    puts "Combien d'étages veux-tu?"
+    print">"
+    stares=gets.chomp.to_i
+
+    aste="#"
+    sky=stares-1
+
+    while(sky>=0)
+        print " "*sky
+        print aste
+        puts " "*sky
+        aste+="##"
+        sky-=1
+    end
+
+end 
+
+def perform
+    full_pyramid
+end
+
+perform
